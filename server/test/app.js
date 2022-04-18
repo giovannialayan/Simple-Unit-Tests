509d595a9dea12f1f37cbe82f46c9dc2e3ead1ee
Giovanni Alayan (RIT Student) <gna7400@rit.edu>
2022-04-18 14:42:02 -0400

    unit testing class example

--- server/test/app.js
+++ server/test/app.js
@@ -1,1 +1,12 @@
 require('./test_config.js');
+require('chai').should();
+
+const app = require('../src/app.js');
+
+describe('server', () => {
+  it('should start up the server', (done) => {
+    app.should.not.be.an('undefined');
+
+    done();
+  });
+});
